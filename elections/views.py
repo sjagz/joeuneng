f9bbc3adfe38085fc94eb1e74da2914334d3e187
dongsu <sjagz@naver.com>
2019-12-07 20:00:35 +0900

    third push

--- elections/views.py
+++ elections/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 def index(request):
 
-	return HttpResponse("hello world , joeuneng first")
+	return HttpResponse("hello world , joeuneng first , git changed finish")
